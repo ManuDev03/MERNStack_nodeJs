6741b3bc3f0143e2213cd92a031a663d39bb0963
ManuDev03 <manuprajan03@gmail.com>
2021-05-12 16:52:15 +0530

    connect to mongodb_create model_Route files using Router

---
+++ src/db/mongoose.js
@@ -0,0 +1,14 @@
+const mongoose = require('mongoose')
+
+mongoose.connect('mongodb://127.0.0.1:27017/MERN', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useUnifiedTopology: true 
+})
+
+.then(() => {
+    console.log('mongodb connected...');
+})
+.catch((err) => {
+    console.log(err.message);
+})
---
+++ src/index.js
@@ -0,0 +1,23 @@
+const express = require('express')
+const app = express()
+const port = process.env.port || 3000
+require('./db/mongoose')
+
+
+app.use('/api/users' ,require('./routes/api/users'))
+app.use('/api/profile' ,require('./routes/api/profile'))
+app.use('/api/posts' ,require('./routes/api/posts'))
+app.use('/api/auth' ,require('./routes/api/auth'))
+
+
+
+
+app.get('/', (req,res) => {
+    res.send('express is running')
+})
+
+app.listen(port,() => {
+    console.log("server is running on port "+ port)
+
+})
+
